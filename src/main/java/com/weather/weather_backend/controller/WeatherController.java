e8e8920101afab62650ad394d490d09ca653622e
Miroslav Musil <xmusil31@mendelu.cz>
2026-08-26 14:39:23 +0200

    Upraven controller pro JSON z MacroDroidu

--- src/main/java/com/weather/weather_backend/controller/WeatherController.java
+++ src/main/java/com/weather/weather_backend/controller/WeatherController.java
@@ -1,4 +1,4 @@
-package com.weather.weatherbackend.controller;
+package com.weather.weather_backend.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
